c0c1e2233e6d8ac3b60a23d48c972fa643c7e434
Sam <sam.weintraub24@gmail.com>
2020-05-10 21:58:08 -0400

    TBRRT and test

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np
-from tqdm import tqdm
 import control
 import pdb
 import matplotlib.pyplot as plt
+import importlib
 
+import scipy.linalg as sp_linalg
 
 # class BallisticMotion():
 #     def __init__(self):
@@ -26,104 +27,47 @@ import matplotlib.pyplot as plt
 class Node:
     def __init__(self, state_time = np.zeros(7, dtype=float)):
         
-        self.hasDynamics = False
         self.hasLQR = False
-
         self.state_time = state_time
-        self.M = None
-        self.C = None
-        self.G = None
 
         #lqr params
         self.K = None
         self.S = None
+        self.u = np.zeros(3)
 
         self.parent = np.nan
         self.child = np.nan
-    
-
 
 class TBRRT():
-    def __init__(self, start, goal):
+    def __init__(self, arm, start, goal):
         """
         :param start: robot arm starting position
         :param goal: goal position
         """
-        
+        self.arm = arm
         self.Tree = []
         self.edge_costs = {} # (parent, child) -> cost
 
         # Store variables
-        self.dt = 0.2
+        self.dt = arm.dt
 
         self.goal = goal
-        self.min_state_time = np.array([0, -5*np.pi/6, -5*np.pi/6, -20, -20, -20, 0])
-        self.max_state_time = np.array([2*np.pi, 5*np.pi/6, 5*np.pi/6, 20, 20, 20, self.goal[-1]])
         self.eps = np.array([2.8, 0.2, 0.03])
 
-        #store coefficients for the M, C, and G matrices 
-        self.g = -9.8
-
-        #mass of each link
-        self.m1 = 1.87
-        self.m2 = 2.2
-        self.m3 = 2.2
-
-        #distance from centroid to corresponding axis
-        self.l1 = 0.125
-        self.l2 = 0.125
-        self.l3 = 0.25
-
-        #link length
-        self.L1 = 0.25
-        self.L2 = 0.25
-        self.L3 = 0.5
-
-        #masses of joints
-        self.mc1 = 0.3
-        self.mc2 = 0.3
-        
-        #moments of inertia
-        self.J1 = 0.0097395
-        self.J2 = 0.011458
-        self.J3 = 0.045833
-        
-        #frictional torque coefficients
-        self.c1 = 0.7056
-        self.c2 = 0.7056
-        self.c3 = 0.7056
-        
-        self.A11 = self.m1*self.l1**2 + self.J1 + (self.m2 + self.m3 + self.mc1 + self.mc2)*self.L1**2
-        self.A12 = (self.m2*self.l2 + (self.m3 + self.mc2)*self.L2)*self.L1
-        self.A13 = self.m3 * self.l3 * self.L1
-
-        self.A22 = self.m2*self.l2**2 + self.J2 + (self.m3 + self.mc2)*self.L2**2
-        self.A23 = self.m3*self.l3*self.L2
-        self.A33 = self.m3*self.l3**2 + self.J3
-        
-        self.B11 = -(self.c1 + self.c2)
-        self.B12 = self.c2 + (self.m2*self.l2 + (self.m3 + self.mc2)*self.L2)*self.L1
-        self.B13 = self.m3 * self.l3 * self.L1
-        
-        self.B21 = self.c2 - (self.m2*self.l1 + (self.m3 + self.mc2)*self.L2)*self.L1
-        self.B22 = -(self.c2 + self.c3)
-        self.B23 = self.c3 + self.m3*self.l3*self.L2
-
-        self.B32 = self.c3 - self.m3*self.l3*self.L2
-        self.B33 = -self.c3
-
-        self.C1 = (self.m1*self.l1 + (self.m2 + self.m3 + self.mc1 + self.mc2)*self.L1)*self.g
-        self.C2 = (self.m2*self.l2 + (self.m3 + self.mc2)*self.L2)*self.g
-        self.C3 = self.m3*self.l3*self.g
-
-        self.Q = np.eye(6) * 100
-        self.R = np.eye(2) * 10
+        self.Q = np.zeros((6,6))
+        self.Q[:3,:3] = np.eye(3) * 1000
+        self.R = np.eye(3) * 0.001
 
         # Add start node to the tree
         start_node = Node(start)
-        self.gen_dynamics(start_node)
-        self.gen_lqr(start_node)
+        self.gen_lqr_new(start_node)
         self.Tree.append(start_node)
+
+        self.min_state_time = np.array([0,0,0,-1,-1,-1,0])
+        self.max_state_time = np.array([2*np.pi,2*np.pi,2*np.pi,1,1,1,self.goal[-1]])
+
+        self.minUs = np.array([-1000,-1000,-1000])
+        self.maxUs = np.array([1000,1000,1000]) * 100
 
     def sample(self):
         """
@@ -132,8 +76,7 @@ class TBRRT():
         rand_state_time = np.random.uniform(self.min_state_time, self.max_state_time)
         xRand = Node(rand_state_time)
 
-        self.gen_dynamics(xRand)
-        self.gen_lqr(xRand)
+        self.gen_lqr_new(xRand)
         
         return xRand
 
@@ -160,15 +103,17 @@ class TBRRT():
             # if abs(np.arctan2(np.sin(node.state_time[2] - xRand.state_time[2]), np.cos(node.state_time[2] - xRand.state_time[2]))) > 0.262:
             #     continue
 
-            #find the cost to go from the neighbor to xRand
-            v_minus_x = np.matrix((node.state_time[0:-1] - xRand.state_time[0:-1]))
+            #check that all 3 angles are w/in 15 degrees of the node in consideration before weighing its LQR cost
+            
+            if np.any(self.wrap(node.state_time[0:3], xRand.state_time[0:3]) > np.ones(3)*np.deg2rad(15.0)):
+                #find the cost to go from the neighbor to xRand
+                v_minus_x = np.matrix(self.wrap(node.state_time[0:-1], xRand.state_time[0:-1]))
 
-            cost = np.matmul(np.matmul(v_minus_x,xRand.S), v_minus_x.T)
+                cost = np.matmul(np.matmul(v_minus_x, xRand.S), v_minus_x.T)
 
-            if cost < minCost:
-                minNode = node
-                minCost = cost
-
+                if cost < minCost:
+                    minNode = node
+                    minCost = cost
 
         return minNode #This should be the state-time vector
 
@@ -177,12 +122,12 @@ class TBRRT():
         Select ui to move from xi toward xrand
         """
         #use the K matrix from xi, and the difference in state should be (xRand-xi)
-        x_del = (xi.state_time[0:-1]- xRand.state_time[0:-1]).reshape(6,1)
-        # WE DIDNT DO WRAPPING...
-                
-        ui = -1 * np.matmul(xi.K, x_del)
+        x_del = self.wrap(xi.state_time[0:-1] , xRand.state_time[0:-1]).reshape(6,1)
 
-        print(ui)
+        ui = np.array(-1 * np.matmul(xi.K, x_del)).reshape((3,))
+
+       # ui_clipped = np.clip(ui,self.minUs,self.maxUs)
+
         #apply control limits to ui 
        # ui_clipped = np.clip(ui,self.minUs, self.maxUs)
 
@@ -223,6 +168,43 @@ class TBRRT():
         path.append(node)
         return path.reverse()
 
+    def gen_lqr_new(self, node):
+        
+        node.hasLQR = True
+
+        u = np.zeros((3,))
+
+        x = node.state_time[0:-1]
+
+        eps = 0.00001  # finite difference epsilon
+        #----------- compute xdot_x and xdot_u using finite differences --------
+        # NOTE: here each different run is in its own column
+        x1 = np.tile(x, (6,1)).T + np.eye(6) * eps
+        x2 = np.tile(x, (6,1)).T - np.eye(6) * eps
+        uu = np.tile(u, (6,1))
+
+        f1 = self.plant_dynamics(x1, uu)
+        f2 = self.plant_dynamics(x2, uu)
+
+        xdot_x = (f1 - f2) / 2 / eps
+   
+        xx = np.tile(x, (3,1)).T 
+        u1 = np.tile(u, (3,1)) + np.eye(3) * eps
+        u2 = np.tile(u, (3,1)) - np.eye(3) * eps
+
+        f1 = self.plant_dynamics(xx, u1)
+        f2 = self.plant_dynamics(xx, u2)
+
+        xdot_u = (f1 - f2) / 2 / eps
+
+        self.A = xdot_x
+        self.B = xdot_u
+
+        node.S = sp_linalg.solve_discrete_are(self.A, self.B, self.Q, self.R)
+        node.K = np.dot(np.linalg.pinv(self.R + np.dot(self.B.T, np.dot(node.S, self.B))), np.dot(self.B.T, np.dot(node.S, self.A)))
+
+        #node.K, node.S, _ = control.lqr(self.A, self.B, self.Q, self.R)
+
     def gen_lqr(self, node):
 
         """ 
@@ -232,12 +214,12 @@ class TBRRT():
         """
         node.hasLQR = True
 
-        control_signal = np.zeros([2,1])
+        control_signal = node.u
         currState = node.state_time[0:6]
 
-        eps = 0.1
+        eps = 0.00001
 
-        time_step = 0.1
+        time_step = self.dt
         
         A = np.zeros([len(currState),len(currState)])
         for i in range(len(currState)):
@@ -247,7 +229,7 @@ class TBRRT():
             x = currState.copy()
             x[i] -= eps
             x_dec = self.simulate_system(node, x, control_signal, time_step)
-            A[:, i] = (x_inc - x_dec) / (2*eps)
+            A[:, i] = self.wrap(x_inc, x_dec) / (2*eps)
 
         B = np.zeros([len(currState), len(control_signal)])
         for i in range(len(control_signal)):
@@ -257,109 +239,100 @@ class TBRRT():
             u = control_signal.copy()
             u[i] -= eps
             x_dec = self.simulate_system(node, currState, u, time_step)
-            B[:,i] = (x_inc - x_dec) / (2*eps)
+            B[:,i] = self.wrap(x_inc, x_dec) / (2*eps)
         
-        node.K, node.S, E = control.lqr(A, B, self.Q, self.R)
+        node.K, node.S, _ = control.lqr(A, B, self.Q, self.R)
     
-    def gen_dynamics(self, node):
-
-        node.hasDynamics = True
+    def wrap(self, target, source):
+        """
+        Returns wrapped angle
+        """
         
-        #thetas
-        t1, t2, t3 = node.state_time[0], node.state_time[1], node.state_time[2]
-        
-        #theta dots
-        td1, td2, td3 = node.state_time[3], node.state_time[4], node.state_time[5]
-
-        node.M = np.matrix([[self.A11, self.A12 * np.cos(t2 - t1), self.A13 * np.cos(t3 - t1)],
-                        [self.A12 * np.cos(t2-t1), self.A22, self.A23 * np.cos(t3 - t2)],
-                        [self.A13 * np.cos(t3 - t1), self.A23*np.cos(t3 - t2), self.A33]])
-
-        
-        node.C = np.matrix([[self.B11, self.B12*np.sin(t2-t1)*td2, self.B13 * np.sin(t3-t1)*td3],
-                        [self.B21*np.sin(t2-t1)*td1, self.B22, self.B23*np.sin(t3-t2)*td3],
-                        [-self.B13*np.sin(t3-t1)*td1, self.B32*np.sin(t3-t2)*td2, self.B33]])
-        
-        node.G = np.matrix([[self.C1*np.sin(t1)],[self.C2*np.sin(t2)],[self.C3*np.sin(t3)]])
-
-
+        #if you are only checking angular difference
+        if len(target) == 3:
+            return np.arctan2(np.sin(target[0:3]-source[0:3]), np.cos(target[0:3]-source[0:3]))
+        else:
+            return np.append(np.arctan2(np.sin(target[0:3]-source[0:3]), np.cos(target[0:3]-source[0:3])), target[3:] - source[3:])
+    
     def simulate_system(self, node, state, input, time_step = 0.01): 
         """
         Simulate system. Only works on state, NOT state time
         """
-        #velocity component
-        x_dot = state[3:6].reshape(3,1)
-        # D = np.matrix([-input[0]],[input[0]-input[1]],[input[1]])
-        D = np.array([-input[0],input[0]-input[1], input[1]]).reshape(-1, 1)
-        
-        z_ddot = np.matmul(np.linalg.inv(node.M), (np.matmul(node.C, x_dot) + node.G + D))
-        updateVec = np.append(x_dot, z_ddot)
 
-        new_state = state + updateVec * time_step        
-        return new_state
+        #set arm position to x
+        self.arm.reset(q=state[0:3],dq=state[3:6])
 
-    def search(self, max_samples):
+        #apply the control signal
+        self.arm.apply_torque(input,time_step)
+
+        #get the next step from the arm 
+        xnext = np.append(np.copy(self.arm.q),np.copy(self.arm.dq))
+
+        return xnext
+    
+    def plant_dynamics(self, x, u):
+
+        """ Simulate the arm dynamics locally. """
+
+        xnext = np.zeros((x.shape))
+
+        for ii in range(x.shape[1]):
+            # set the arm position to x
+            self.arm.reset(q=x[:3, ii], 
+                          dq=x[3:6, ii])
+
+            # apply the control signal
+            # TODO: should we be using a constant timestep here instead of arm.dt?
+            # to even things out when running at different dts? 
+
+            self.arm.apply_torque(u[ii], self.arm.dt)
+            # get the system state from the arm
+            xnext[:,ii] = np.hstack([np.copy(self.arm.q), 
+                                   np.copy(self.arm.dq)])
+
+        return xnext
+
+    def search(self, max_samples = 10000):
         """
         Perform TB-RRT Algorithm
         :param max_samples: Number of samples until termination 
         """
-        for k in range(max_samples):
-            print(k)
-
+        while (len(self.Tree) < max_samples):
             xRand = self.sample()
             xi = self.nearest_neighbor(xRand)
+            
+            if(xi == None):
+                # print("Too far")
+                continue
+
             xi_1, ui = self.steer(xi, xRand)
+
+            if (np.any(abs(ui) > self.maxUs)):
+                #print("Too much control")
+                continue
 
             if self.is_reachable(xi_1):
                 xi_1 = np.append(xi_1, xi.state_time[-1]+self.dt)
                 xi_1_node = Node(xi_1)
+                xi_1_node.u = ui
 
-                #potentially only need gen_dynamics if we are steering
-                self.gen_dynamics(xi_1_node)
-                self.gen_lqr(xi_1_node)
-
+                self.gen_lqr_new(xi_1_node)                
                 self.Tree.append(xi_1_node)
-                self.add_edge(xi, xi_1_node, ui)
+                if len(self.Tree) % 10 == 0:
+                    print(len(self.Tree))
+                self.add_edge(xi, xi_1_node, np.linalg.norm(ui))
+                
                 if self.near_goal(xi_1):
                     path = self.get_path(xi_1_node)
                     return path
-    
-    def test_dynamics(self):
-        t1s = []
-        t2s = []
-        t3s = []
-        t1ds = []
-        t2ds = []
-        t3ds = []
 
-        node = Node(np.array([0.1, 0., 0., 0., 0., 0., 0.]))
-        self.gen_dynamics(node)
-        self.gen_lqr(node)
+                if len(self.Tree) % 150 == 0:
+                    goal_node = Node(self.goal)
+                    self.gen_lqr_new(goal_node)
+                    nearest_to_goal = self.nearest_neighbor(goal_node)
+                    pdb.set_trace()
         
-        for t in range(1000):
-            new_state = self.simulate_system(node, node.state_time[0:6], [0.0,0.0], time_step = 0.01)
-
-            t1s.append(new_state[0])
-            t2s.append(new_state[1])
-            t3s.append(new_state[2])
-            t1ds.append(new_state[3])
-            t2ds.append(new_state[4])
-            t3ds.append(new_state[5])
-
-            node = Node(np.append(new_state, t))
-            self.gen_dynamics(node)
-            self.gen_lqr(node)
-        
-        a = plt.plot(t1s)
-        plt.savefig('t1')
-
-        plt.figure()
-        b = plt.plot(t2s)
-        plt.figure()
-        plt.plot(t3s)
-
-   
-
+        print("No path found")
 
 """
 - "Nearest neighbor" is the state in the tree that requires the least amount of control effort to get to x_rand. 
@@ -380,6 +353,11 @@ dictionary which contains the 6d vector of actual joint torques during this tran
 
 if __name__=='__main__':
     
+    subfolder = 'three_link'
+    arm_name = 'arms.%s.%s' % (subfolder, 'arm')
+    arm_module = importlib.import_module(name=arm_name)
+    arm = arm_module.Arm(dt=0.01)
+
     #state = theta (degrees), theta_dot, time
     
     #Start position 
@@ -389,6 +367,5 @@ if __name__=='__main__':
     end = np.array([0, 0.5, 0.5, 0, 0.5, 0.5, 3.0]) # This is random...
 
     #intialize Tree with Xo (starting point)
-    rrt = TBRRT(start,end)
-    rrt.test_dynamics()
-    #path = rrt.search(10000)
+    rrt = TBRRT(arm, start,end)
+    path = rrt.search(10000)
